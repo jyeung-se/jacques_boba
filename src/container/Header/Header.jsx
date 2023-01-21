3dacee7e544fcb796f3e75bb25d5ad2f1e0c83f5
Jack Yeung <jyeung.se@gmail.com>
2023-01-21 02:24:51 -0500

    Finished header and subheader components.

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,10 +9,12 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title='Chase the new flavor' />
       <h1 className='app__header-h1'>Secrets to an Out of World Experience</h1>
+      <p className='p__opensans' style={{ margin: '2rem 0' }}>Taste the marvelous and world class culinary dishes of Jacques</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
     </div>
 
     <div className='app__wrapper_img'>
-
+      <img src={images.welcome} alt='header img' />
     </div>
     Header
   </div>

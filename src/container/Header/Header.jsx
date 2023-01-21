6299e61d96edc32c958dc26f39c24c1eac8d8be9
Jack Yeung <jyeung.se@gmail.com>
2023-01-21 02:08:44 -0500

    started header and subheader components.

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from 'react'
 
-import './Header.css';
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
+import './Header.css'
 
 const Header = () => (
-  <div>
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Chase the new flavor' />
+      <h1 className='app__header-h1'>Secrets to an Out of World Experience</h1>
+    </div>
+
+    <div className='app__wrapper_img'>
+
+    </div>
     Header
   </div>
 );

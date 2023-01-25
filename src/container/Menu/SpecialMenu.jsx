208142b41a82c0f46148fe9faeb9244014160878
Jack Yeung <jyeung.se@gmail.com>
2023-01-24 23:02:57 -0500

    styled todays special menu item drink names and ingredients

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -13,7 +13,7 @@ const SpecialMenu = () => (
 
     <div className='app__specialMenu-menu'>
       <div className='app__specialMenu-menu_wine flex__center'>
-        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
         <div className='app__specialMenu-menu-items'>
           {data.wines.map((wine, index) => (
             // <p>{wine.title}</p>
@@ -27,7 +27,7 @@ const SpecialMenu = () => (
       </div>
 
       <div className='app__specialMenu-menu_cocktails flex__center'>
-        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <p className='app__specialMenu-menu_heading'>Cocktails</p>
         <div className='app__specialMenu-menu-items'>
           {data.cocktails.map((cocktail, index) => (
             <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} /> 

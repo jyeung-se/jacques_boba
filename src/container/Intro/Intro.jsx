1d0f1bd5eec63b0d33cc3b4ff9455a2ca470236c
Jack Yeung <jyeung.se@gmail.com>
2023-01-26 21:54:31 -0500

    added video intro placeholder media

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { React, useState, useRef } from 'react';
+import { BsFillPlayFill, BsPauseFill} from 'react-icons'
 
+import { meal } from '../../constants'
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false)
+  const vidRef = useRef()
+
+  return (
+    <div className='app__video'>
+    <video 
+    src={meal}
+    ref={vidRef}
+    type='video/mp4'
+    loop
+    controls={false}
+    muted
+    />
   </div>
-);
+  )
+}
 
 export default Intro;

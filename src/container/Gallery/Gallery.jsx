ff53c9ea02e25b192c93119244ab9839ecbd7e8c
Jack Yeung <jyeung.se@gmail.com>
2023-01-30 23:53:54 -0500

    Begin gallery portion of page

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsRightArrowShort } from 'react-icons/bs'
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants'
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+
+  return (
+    <div>
+      Gallery stuff here
+    </div>
+  )
+
+}
 
 export default Gallery;

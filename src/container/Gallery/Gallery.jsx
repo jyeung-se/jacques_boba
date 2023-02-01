ec077e3fb23e99cbca158b96d52dd41ff168d4b1
Jack Yeung <jyeung.se@gmail.com>
2023-01-31 22:24:58 -0500

    Added left and right arrow buttons for scrolling through images in gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import { BsInstagram, BsArrowLeftShort, BsRightArrowShort } from 'react-icons/bs'
+import { React, useRef } from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs'
 
 import { SubHeading } from '../../components';
 import { images } from '../../constants'
 import './Gallery.css';
 
 const Gallery = () => {
+  const scrollRef = useRef(null)
+
+  const scroll = (direction) => {
+    const { current } = scrollRef
+
+    if(direction === 'left') {
+      current.scrollLeft -= 300
+    } else {
+      current.scrollRight += 300
+    }
+  }
 
   return (
     <div className='app__gallery flex__center'>
@@ -13,6 +24,16 @@ const Gallery = () => {
         <SubHeading title='Instagram' />
         <h1 className='headtext__cormorant'>Photo Gallery</h1>
         <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem' }}>Photo gallery place holder text and description etc here.</p>
+        <button type='button' className='custom__button'>View More</button>
+      </div>
+
+      <div className='app_gallery-images'>
+        <div className='app__gallery-images_container' ref={scrollRef}>
+        </div>
+        <div className='app__gallery-images_arrow'>
+          <BsArrowLeftShort className='gallery__arrow-icon' onClick={( )=> scroll('left')} />
+          <BsArrowRightShort className='gallery__arrow-icon' onClick={( )=> scroll('right')} />
+        </div>
       </div>
     </div>
   )

5fec45287e7c926215f151dc7ef6af84d0bf6c95
Jack Yeung <jyeung.se@gmail.com>
2023-02-04 22:24:24 -0500

    Updated aboutus text

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -22,7 +22,7 @@ const AboutUs = () => (
       <div className='app__aboutus-content_history'>
         <h1 className='headtext__cormorant'>Our History</h1>
         <img src={images.ribbonstraight} alt='about_ribbon' className='ribbonstraight__img' />
-        <p className='p__opensans'>Boba Master who has explored the world's finest and uniqueness to bring you an untasted experience</p>
+        <p className='p__opensans'>Prior to The Jacques's founding in 2020, Our founder Jack had set out on a mission to taste the infinite ingredients and flavors of the world. With all the possibilities gathered, Jack created new flavors with his expertise in mixology. With his developed palette, he opened The Jacques to share his creations with the world! </p>
         <button type='button' className='custom__button'>Reveal More</button>
       </div>
 

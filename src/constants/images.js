4ed930566de662c9ed36f953c6f022939599f626
Jack Yeung <jyeung.se@gmail.com>
2023-02-04 22:08:05 -0500

    englarged margin space inbetween the aboutus section

--- src/constants/images.js
+++ src/constants/images.js
@@ -5,7 +5,6 @@ import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
 import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
 import logo from '../assets/logo.png';
 import menu from '../assets/menu.png';
 import overlaybg from '../assets/overlaybg.png';
@@ -16,14 +15,12 @@ import award01 from '../assets/award01.png';
 import award02 from '../assets/award02.png';
 import award03 from '../assets/award03.png';
 import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
 import bobamenupic from '../assets/bobamenupic.png';
 import chefjy from '../assets/chefjy.png';
 import signjy from  '../assets/signjy.png';
 import jacques from  '../assets/jacques.png';
-import bobastraw from  '../assets/bobastraw.png';
 import J from  '../assets/J.png';
 import goldribbon from '../assets/goldribbon.png';
 import redboba from '../assets/redboba.png';
@@ -34,6 +31,7 @@ import watermelonboba from '../assets/watermelonboba.png';
 import mangoboba from '../assets/mangoboba.png';
 import galaxyboba from '../assets/galaxyboba.png';
 import moonawardicon from '../assets/moonawardicon.png';
+import ribbonstraight from '../assets/ribbonstraight.png';
 
 
 
@@ -45,7 +43,6 @@ export default {
   gallery02,
   gallery03,
   gallery04,
-  knife,
   logo,
   menu,
   overlaybg,
@@ -56,14 +53,12 @@ export default {
   award02,
   award03,
   award05,
-  sign,
   quote,
   gericht,
   bobamenupic,
   chefjy,
   signjy,
   jacques,
-  bobastraw,
   J,
   goldribbon,
   redboba,
@@ -73,5 +68,6 @@ export default {
   watermelonboba,
   mangoboba,
   galaxyboba,
-  moonawardicon
+  moonawardicon,
+  ribbonstraight
 };
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -11,18 +11,17 @@ const AboutUs = () => (
     <div className='app__aboutus-content flex__center'>
       <div className='app__aboutus-content_about'>
         <h1 className='headtext__cormorant'>About Us</h1>
-        <img src={images.spoon} alt='about_spoon' className='spoon__img' />
+        <img src={images.ribbonstraight} alt='about_ribbon' className='ribbonstraight__img' />
         <p className='p__opensans'>Boba Master who has explored the world's finest and uniqueness to bring you an untasted experience</p>
         <button type='button' className='custom__button'>Reveal More</button>
       </div>
 
-      <div className='app__aboutus-content_knife flex__center'>
-        {/* <img src={images.bobastraw} alt='about_knife' /> */}
+      <div className='app__aboutus-content_space flex__center'>
       </div>
 
       <div className='app__aboutus-content_history'>
         <h1 className='headtext__cormorant'>Our History</h1>
-        <img src={images.spoon} alt='about_spoon' className='spoon__img' />
+        <img src={images.ribbonstraight} alt='about_ribbon' className='ribbonstraight__img' />
         <p className='p__opensans'>Boba Master who has explored the world's finest and uniqueness to bring you an untasted experience</p>
         <button type='button' className='custom__button'>Reveal More</button>
       </div>
